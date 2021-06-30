1f42a39912422891eadba80a97882a1e438e6179
Badeca <gchf@cin.ufpe.br>
2021-06-30 18:58:00 -0300

    animação ao rolar a página

--- scripts.js
+++ scripts.js
@@ -40,3 +40,22 @@ const swiper = new Swiper('.swiper-container', {
   mousewheel: true,
   keyboard: true
 })
+
+/* ScrollReveal */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+  #home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonals,
+  #contact .text, #contact .links
+`,
+  { interval: 100 }
+)

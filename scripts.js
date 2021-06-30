cbd11b9cd96c013d01a27fd8e991840fb57cb1e7
Badeca <gchf@cin.ufpe.br>
2021-06-30 02:37:20 -0300

    adicionados Serviços e Sobre nós, sombra no header ao scroll da página, efeito para transição do menu + rolagem smooth

--- scripts.js
+++ scripts.js
@@ -18,3 +18,15 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+
+/* Adiciona sombra no header ao dar scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
